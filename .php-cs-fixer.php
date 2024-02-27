6322120894bcfb5477fb1e45244c64ebf060d88c
Leon Boot <leon@nedbase.nl>
2024-02-27 15:23:06 +0100

    PHP CS Fixer configuration

---
+++ .php-cs-fixer.php
@@ -0,0 +1,13 @@
+<?php
+
+$finder = (new PhpCsFixer\Finder())
+    ->in([
+        'src',
+        'tests',
+    ]);
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@Symfony' => true,
+    ])
+    ->setFinder($finder);
